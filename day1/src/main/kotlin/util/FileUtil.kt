1d0d70c48c8141a6642bd96d9432903dd8b46bcf
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2020-12-01 23:49:30 +0100

    Day 1

---
+++ day1/src/main/kotlin/util/FileUtil.kt
@@ -0,0 +1,12 @@
+package util
+
+class FileUtil {
+    fun readResource(path: String): String? =
+        when (val data = this::class.java.getResource(path)?.readText(Charsets.UTF_8)) {
+            is String -> data
+            else -> {
+                println("Failed to read resource $path")
+                null
+            }
+        }
+}

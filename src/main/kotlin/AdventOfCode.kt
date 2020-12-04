8655982a902cf0c1732d69009f81f6dc54b8e7cb
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2020-12-04 19:46:11 +0100

    Add day 4 to main list

--- src/main/kotlin/AdventOfCode.kt
+++ src/main/kotlin/AdventOfCode.kt
@@ -5,6 +5,7 @@ val daysList = listOf(
     Day(1, "Report Repair", listOf(days.day1.Task1(), days.day1.Task2())),
     Day(2, "Password Philosophy", listOf(days.day2.Task1(), days.day2.Task2())),
     Day(3, "Toboggan Trajectory", listOf(days.day3.Task1(), days.day3.Task2())),
+    Day(4, "Passport Processing", listOf(days.day4.Task1(), days.day4.Task2())),
 )
 
 fun main() {

362b68c3eaab88d6da9d4d6fe898fb45ab9d8e74
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2021-12-02 19:31:51 +0100

    Add test setup; Add tests for day 1 task 1

---
+++ 2021/src/tasks/__tests__/day1.ts
@@ -0,0 +1,17 @@
+import { task1 } from "../day1";
+import * as path from "path";
+import { loadData } from "../../util";
+
+describe("day1", () => {
+  describe("task1", () => {
+    it("gives the right answer for test data a", () => {
+      const data = loadData(path.resolve(__dirname, "data/day1/task1a"));
+      expect(task1(data)).toBe(7);
+    });
+
+    it("gives the right answer for the input", () => {
+      const data = loadData("data/day1");
+      expect(task1(data)).toBe(1266);
+    });
+  });
+});
--- 2021/src/tasks/index.ts
+++
@@ -1,7 +0,0 @@
-import { task1 as day1task1 } from "./day1/task1";
-
-export const tasks = {
-  day1: {
-    task1: day1task1,
-  },
-};

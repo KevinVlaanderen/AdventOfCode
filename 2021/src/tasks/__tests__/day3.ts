b5b6346523eba010421468a91710e8f179c03feb
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2021-12-03 20:14:45 +0100

    Day 3 task 2

--- 2021/src/tasks/__tests__/day3.ts
+++ 2021/src/tasks/__tests__/day3.ts
@@ -1,4 +1,4 @@
-import { task1 } from "../day3";
+import { task1, task2 } from "../day3";
 import * as path from "path";
 import { loadData } from "../../util";
 
@@ -11,5 +11,17 @@ describe("task1", () => {
   it("gives the right answer for the input", () => {
     const data = loadData("data/day3");
     expect(task1(data)).toBe(3847100);
+  });
+});
+
+describe("task2", () => {
+  it("gives the right answer for test data a", () => {
+    const data = loadData(path.resolve(__dirname, "data/day3/task2a"));
+    expect(task2(data)).toBe(230);
+  });
+
+  it("gives the right answer for the input", () => {
+    const data = loadData("data/day3");
+    expect(task2(data)).toBe(4105235);
   });
 });
--- 2021/src/tasks/day3.ts
+++ 2021/src/tasks/day3.ts
@@ -4,7 +4,7 @@ import { zip } from "../util";
 export const task1: Task = (data) => {
   const amount = data.length;
   const sums = data
-    .map((number) => Array.from(number).map((bit) => parseInt(bit, 10)))
+    .map((number) => Array.from(number).map((bit) => parseInt(bit, 2)))
     .reduce((previous, current) =>
       zip(previous, current).map(([a, b]) => a + b)
     );
@@ -17,3 +17,34 @@ export const task1: Task = (data) => {
 
   return gamma * epsilon;
 };
+
+export const task2: Task = (data) => {
+  const numbers = data.map((number) =>
+    Array.from(number).map((bit) => parseInt(bit, 2))
+  );
+
+  const oxygenGeneratorRating = parseInt(filter(numbers, "most").join(""), 2);
+  const co2ScrubberRating = parseInt(filter(numbers, "least").join(""), 2);
+
+  return oxygenGeneratorRating * co2ScrubberRating;
+};
+
+function filter(
+  data: Array<number[]>,
+  type: "least" | "most",
+  index: number = 0
+): number[] {
+  if (!data || index >= data[0].length) {
+    throw new Error();
+  }
+
+  const amount = data.length;
+  const sum = data.reduce((previous, current) => current[index] + previous, 0);
+  const criteria =
+    type === "least" ? (sum < amount / 2 ? 1 : 0) : sum >= amount / 2 ? 1 : 0;
+  const filteredData = data.filter((item) => item[index] === criteria);
+
+  return filteredData.length === 1
+    ? filteredData[0]
+    : filter(filteredData, type, index + 1);
+}

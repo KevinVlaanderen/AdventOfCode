81ec321b32eb8ad7a334b56f640b555c79bef7ff
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2021-12-09 22:19:47 +0100

    Day 7 task 2

--- 2021/src/tasks/day7.ts
+++ 2021/src/tasks/day7.ts
@@ -3,12 +3,29 @@ import { range } from "../util";
 
 export const task1: Task = (data) => {
   const positions = data.split(",").map((item) => parseInt(item, 10));
+
+  return calculateCosts(positions, (distance) => distance)[0];
+};
+
+export const task2: Task = (data) => {
+  const positions = data.split(",").map((item) => parseInt(item, 10));
+
+  return calculateCosts(
+    positions,
+    (distance) => (distance * (distance + 1)) / 2
+  )[0];
+};
+
+function calculateCosts(
+  positions: number[],
+  costFn: (distance: number) => number
+) {
   return range(0, Math.max(...positions), 1)
     .map((index) =>
       positions.reduce(
-        (result, current) => result + Math.abs(current - index),
+        (result, current) => result + costFn(Math.abs(current - index)),
         0
       )
     )
-    .sort((a, b) => a - b)[0];
-};
+    .sort((a, b) => a - b);
+}

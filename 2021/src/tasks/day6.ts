7b1eb2a20372cd89ec4cd7d9dcc1e7559955f684
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2021-12-09 21:06:26 +0100

    Day 6 task 2

--- 2021/src/tasks/day6.ts
+++ 2021/src/tasks/day6.ts
@@ -2,13 +2,30 @@ import { Task } from "../types";
 import { range } from "../util";
 
 export const task1: Task = (data) => {
-  const fish = data.split(",").map((item) => parseInt(item, 10));
+  return breed(
+    data.split(",").map((item) => parseInt(item, 10)),
+    80
+  );
+};
 
-  range(1, 80, 1).forEach(() => {
-    const newFish = fish.filter((item) => item === 0).map(() => 8);
-    fish.forEach((item, index) => (fish[index] = item === 0 ? 6 : item - 1));
-    fish.push.apply(fish, newFish);
+export const task2: Task = (data) => {
+  return breed(
+    data.split(",").map((item) => parseInt(item, 10)),
+    256
+  );
+};
+
+function breed(fish: number[], iterations: number) {
+  const buckets = Array.from(
+    { length: 9 },
+    (_, index) => fish.filter((item) => item === index).length
+  );
+
+  range(1, iterations, 1).forEach(() => {
+    const newFish = buckets.shift()!;
+    buckets.push.apply(buckets, [newFish]);
+    buckets[6] += newFish;
   });
 
-  return fish.length;
-};
+  return buckets.reduce((result, current) => result + current);
+}

a1f61b6c7f193523ecbad72f69c154d81d62dbfe
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2021-12-09 21:06:26 +0100

    Day 6 task 2

--- 2021/src/tasks/__tests__/day6.ts
+++ 2021/src/tasks/__tests__/day6.ts
@@ -1,4 +1,4 @@
-import { task1 } from "../day6";
+import { task1, task2 } from "../day6";
 import * as path from "path";
 import { loadData } from "../../util";
 
@@ -11,5 +11,17 @@ describe("task1", () => {
   it("gives the right answer for the input", () => {
     const data = loadData("data/day6");
     expect(task1(data)).toBe(355386);
+  });
+});
+
+describe("task2", () => {
+  it("gives the right answer for test data a", () => {
+    const data = loadData(path.resolve(__dirname, "data/day6/task2a"));
+    expect(task2(data)).toBe(26984457539);
+  });
+
+  it("gives the right answer for the input", () => {
+    const data = loadData("data/day6");
+    expect(task2(data)).toBe(1613415325809);
   });
 });

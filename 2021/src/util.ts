99dc040860b6982fc88ea321dd5cbcf01e6104dc
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2021-12-13 19:39:16 +0100

    Day 8 task 2

--- 2021/src/util.ts
+++ 2021/src/util.ts
@@ -20,3 +20,11 @@ export function range(start: number, stop: number, step: number) {
     (_, i) => start + i * step * (start > stop ? -1 : 1)
   );
 }
+
+export function subtract<T>(a: Set<T>, ...b: Set<T>[]): Set<T> {
+  return new Set([...a].filter((x) => !b.some((y) => y.has(x))));
+}
+
+export function equal<T>(a: Set<T>, b: Set<T>): boolean {
+  return a.size === b.size && [...a].every((v) => b.has(v));
+}

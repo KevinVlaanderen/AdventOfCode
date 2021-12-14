89579e838a1c1a9beef090ad38a2956400b039b0
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2021-12-14 22:49:09 +0100

    Day 9 task 2

--- 2021/src/tasks/__tests__/day9.ts
+++ 2021/src/tasks/__tests__/day9.ts
@@ -1,4 +1,4 @@
-import { task1 } from "../day9";
+import { task1, task2 } from "../day9";
 import * as path from "path";
 import { loadData } from "../../util";
 
@@ -11,5 +11,17 @@ describe("task1", () => {
   it("gives the right answer for the input", () => {
     const data = loadData("data/day9");
     expect(task1(data)).toBe(452);
+  });
+});
+
+describe("task2", () => {
+  it("gives the right answer for test data a", () => {
+    const data = loadData(path.resolve(__dirname, "data/day9a"));
+    expect(task2(data)).toBe(1134);
+  });
+
+  it("gives the right answer for the input", () => {
+    const data = loadData("data/day9");
+    expect(task2(data)).toBe(1263735);
   });
 });
--- 2021/src/tasks/day9.ts
+++ 2021/src/tasks/day9.ts
@@ -1,4 +1,5 @@
 import { withLines } from "../parsers";
+import { isDefined } from "../util";
 
 export const task1 = withLines((data) => {
   const input = data.map((line) =>
@@ -28,3 +29,81 @@ export const task1 = withLines((data) => {
     )
     .reduce((result, current) => result + current + 1, 0);
 });
+
+type BasinMap = Array<Array<number | undefined | null>>;
+
+export const task2 = withLines((data) => {
+  const input = data.map((line) =>
+    Array.from(line).map((char) => char !== "9")
+  );
+
+  const map: BasinMap = input.map((line) =>
+    line.map((item) => (item ? undefined : null))
+  );
+
+  const groups = [];
+
+  for (let start; (start = findNext(map)); ) {
+    const neighbours = findNeighbours(map, start, []);
+    neighbours.forEach(
+      (neighbour) => (map[neighbour[1]][neighbour[0]] = groups.length)
+    );
+    groups.push(neighbours);
+  }
+
+  return groups
+    .map((group) => group.length)
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((result, current) => result * current, 1);
+});
+
+function findNext(map: BasinMap): [number, number] | undefined {
+  for (const [y, row] of map.entries()) {
+    for (const [x, entry] of row.entries()) {
+      if (entry === undefined) {
+        return [x, y];
+      }
+    }
+  }
+}
+
+const checks = [
+  [-1, 0],
+  [0, -1],
+  [1, 0],
+  [0, 1],
+];
+
+function findNeighbours(
+  map: BasinMap,
+  current: [number, number],
+  previous: Array<[number, number]>
+): Array<[number, number]> {
+  const [x, y] = current;
+  const neighbours = checks
+    .map((check) => [x + check[0], y + check[1]])
+    .filter(
+      ([newX, newY]) =>
+        newX >= 0 && newY >= 0 && newX < map[y].length && newY < map.length
+    )
+    .map<[number, number] | undefined>(([newX, newY]) =>
+      map[newY][newX] === undefined ? [newX, newY] : undefined
+    )
+    .filter(isDefined)
+    .filter(
+      (neighbour) =>
+        !previous.some(
+          (prev) => prev[0] === neighbour[0] && prev[1] === neighbour[1]
+        )
+    );
+
+  previous.push(current, ...neighbours);
+
+  return [
+    current,
+    ...neighbours.flatMap((neighbour) =>
+      findNeighbours(map, neighbour, previous)
+    ),
+  ];
+}

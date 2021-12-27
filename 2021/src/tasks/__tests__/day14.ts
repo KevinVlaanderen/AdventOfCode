f126660f3ec922410c2cf80cdb1f87bcc4c8bd07
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2021-12-28 00:18:37 +0100

    Day 14 task 2

--- 2021/src/tasks/__tests__/day14.ts
+++ 2021/src/tasks/__tests__/day14.ts
@@ -1,7 +1,12 @@
 import { realDataPath, test, testDataPath } from "../../testUtil";
-import { task1 } from "../day14";
+import { task1, task2 } from "../day14";
 
 describe("task 1", () => {
   it("test data", test(task1, testDataPath("day14"), 1588));
   it("real data", test(task1, realDataPath("day14"), 3009));
+});
+
+describe("task 2", () => {
+  it("test data", test(task2, testDataPath("day14"), 2188189693529));
+  it("real data", test(task2, realDataPath("day14"), 3459822539451));
 });
--- 2021/src/tasks/day14.ts
+++ 2021/src/tasks/day14.ts
@@ -1,37 +1,117 @@
 import { withBlocks } from "../parsers";
-import { range, window } from "../util";
+import { window } from "../util";
+
+type Rules = {
+  between: { [key: string]: string };
+  pairs: { [key: string]: [string, string] };
+};
 
 export const task1 = withBlocks((data) => {
-  const rules = parseRules(data[1]);
-
-  const result = range(1, 10).reduce((template, _) => {
-    const pairs = Array.from(window(template, 2));
-
-    return pairs.flatMap((pair, index) => {
-      const newElement = rules[pair.join("")];
-      return index < pairs.length - 1
-        ? [pair[0], newElement]
-        : [pair[0], newElement, pair[1]];
-    });
-  }, data[0].split(""));
-
-  const uniqueElements = new Set(result);
-  const counts = Array.from(uniqueElements)
-    .map<[string, number]>((element) => [
-      element,
-      result.filter((item) => item === element).length,
-    ])
-    .sort((a, b) => b[1] - a[1]);
-
-  return counts[0][1] - counts[counts.length - 1][1];
+  return solve(data, 10);
 });
 
-function parseRules(data: string): { [key: string]: string } {
-  return data.split("\n").reduce((result, line) => {
+export const task2 = withBlocks((data) => {
+  return solve(data, 40);
+});
+
+function solve(data: string[], steps: number): number {
+  const template = data[0];
+  const rules = parseRules(data[1]);
+
+  const counter = new Counter(...template);
+  const cache = new Cache();
+
+  counter.merge(grow(template, rules, steps, cache));
+
+  return calculateScore(counter);
+}
+
+function parseRules(data: string): Rules {
+  const rules: Rules = { between: {}, pairs: {} };
+  data.split("\n").forEach((line) => {
     const matches = line.match(/(..) -> (.)/)!;
-    return {
-      ...result,
-      [matches[1]]: matches[2],
-    };
-  }, {});
+    rules.between[matches[1]] = matches[2];
+    rules.pairs[matches[1]] = [
+      matches[1][0] + matches[2],
+      matches[2] + matches[1][1],
+    ];
+  });
+  return rules;
+}
+
+function grow(
+  template: string,
+  rules: Rules,
+  step: number,
+  cache: Cache
+): Counter {
+  if (step === 0) return new Counter();
+
+  if (cache) {
+    const cachedCounter = cache.get(template, step);
+    if (cachedCounter) return cachedCounter;
+  }
+
+  const counter = new Counter();
+
+  const letters = Array.from(template);
+  for (const [start, end] of window(letters, 2)) {
+    if (rules.pairs[start + end]) {
+      const between = rules.between[start + end];
+      counter.add(between);
+      counter.merge(grow(start + between + end, rules, step - 1, cache));
+    }
+  }
+
+  cache.set(template, step, counter);
+
+  return counter;
+}
+
+function calculateScore(counter: Counter) {
+  const sorted = Object.values(counter.all()).sort((a, b) => b - a);
+  return sorted[0] - sorted[sorted.length - 1];
+}
+
+class Counter {
+  private counts: { [character: string]: number } = {};
+
+  constructor(...character: string[]) {
+    character.forEach((character) => this.add(character));
+  }
+
+  add(key: string): void {
+    this.counts[key] = this.counts[key] ? this.counts[key] + 1 : 1;
+  }
+
+  get(key: string): number {
+    return this.counts[key] ?? 0;
+  }
+
+  all(): { [character: string]: number } {
+    return this.counts;
+  }
+
+  merge(counter: Counter): void {
+    Object.entries(counter.all()).forEach(([character, n]) => {
+      this.counts[character] = this.get(character) + n;
+    });
+  }
+}
+
+class Cache {
+  private cache: { [template: string]: { [step: number]: Counter } } = {};
+
+  set(template: string, step: number, counter: Counter): void {
+    if (this.cache[template] === undefined) {
+      this.cache[template] = {};
+    }
+    if (this.cache[template][step] === undefined) {
+      this.cache[template][step] = counter;
+    }
+  }
+
+  get(template: string, step: number): Counter | undefined {
+    return template in this.cache ? this.cache[template][step] : undefined;
+  }
 }

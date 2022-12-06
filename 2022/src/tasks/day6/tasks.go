8c822b5de5bdc12bdf75dcb2c9f67b69ae22525b
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2022-12-06 10:13:14 +0100

    Day 6 task 2

--- 2022/src/tasks/day6/tasks.go
+++ 2022/src/tasks/day6/tasks.go
@@ -12,27 +12,34 @@ func Task1(filePath string) (result framework.TaskResult[int]) {
 		return
 	}
 
-	for i := 0; i < len(data)-4; i++ {
-		slice := data[i : i+4]
+	marker := findMarker(data, 4)
 
-		var unique uint32
-		for _, char := range slice {
-			unique |= 1 << (int(char) - 97)
-		}
-		if bits.OnesCount32(unique) == 4 {
-			return framework.TaskResult[int]{Value: i + 4, Error: nil}
-		}
-	}
-
-	return
+	return framework.TaskResult[int]{Value: marker}
 }
 
 func Task2(filePath string) (result framework.TaskResult[int]) {
-	_, err := framework.ReadFull(filePath)
+	data, err := framework.ReadFull(filePath)
 	if err != nil {
 		result.Error = err
 		return
 	}
 
-	return
+	marker := findMarker(data, 14)
+
+	return framework.TaskResult[int]{Value: marker}
+}
+
+func findMarker(data string, length int) int {
+	for i := 0; i < len(data)-length; i++ {
+		slice := data[i : i+length]
+
+		var unique uint32
+		for _, char := range slice {
+			unique |= 1 << (int(char) - 97)
+		}
+		if bits.OnesCount32(unique) == length {
+			return i + length
+		}
+	}
+	return -1
 }
--- 2022/src/tasks/day6/tasks_test.go
+++ 2022/src/tasks/day6/tasks_test.go
@@ -36,3 +36,35 @@ func TestTask1(t *testing.T) {
 	t.Run("test data 5", test.CreateTest(Task1, testDataPath5, 11))
 	t.Run("real data", test.CreateTest(Task1, realDataPath, 1042))
 }
+
+func TestTask2(t *testing.T) {
+	var testDataPath1, testDataPath2, testDataPath3, testDataPath4, testDataPath5, realDataPath string
+	var err error
+
+	if testDataPath1, err = test.CreateTestDataPath("data1"); err != nil {
+		t.Fatal(err)
+	}
+	if testDataPath2, err = test.CreateTestDataPath("data2"); err != nil {
+		t.Fatal(err)
+	}
+	if testDataPath3, err = test.CreateTestDataPath("data3"); err != nil {
+		t.Fatal(err)
+	}
+	if testDataPath4, err = test.CreateTestDataPath("data4"); err != nil {
+		t.Fatal(err)
+	}
+	if testDataPath5, err = test.CreateTestDataPath("data5"); err != nil {
+		t.Fatal(err)
+	}
+
+	if realDataPath, err = test.CreateRealDataPath("day6"); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("test data 1", test.CreateTest(Task2, testDataPath1, 19))
+	t.Run("test data 2", test.CreateTest(Task2, testDataPath2, 23))
+	t.Run("test data 3", test.CreateTest(Task2, testDataPath3, 23))
+	t.Run("test data 4", test.CreateTest(Task2, testDataPath4, 29))
+	t.Run("test data 5", test.CreateTest(Task2, testDataPath5, 26))
+	t.Run("real data", test.CreateTest(Task2, realDataPath, 2980))
+}

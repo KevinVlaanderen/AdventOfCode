80fe12f669b9a4a309c62b65c69a80b903ccf14b
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2024-12-11 15:36:54 +0100

    improve performance 2024 9

--- 2024/AoC2024/Sources/Tasks/Day9.swift
+++ 2024/AoC2024/Sources/Tasks/Day9.swift
@@ -10,110 +10,185 @@ public struct Day9: Day {
 
         switch task {
         case .task1:
-            task1(&disk)
+            return checksum(files: task1(&disk))
         case .task2:
-            task2(&disk)
+            return checksum(files: task2(&disk))
         }
-        
-        return disk.checksum()
     }
     
-    private func task1(_ disk: inout Disk) {
-        var endIndex = disk.endIndex-1
+    private func task1(_ disk: inout Disk) -> [Content] {
+        var files: [Content] = []
         
-        for startIndex in 0...endIndex {
-            if case .file(_) = disk[startIndex] {
-                continue
-            }
-            
-            while case .emptySpace = disk[endIndex] {
-                endIndex -= 1
-            }
-            
-            if endIndex <= startIndex {
+        var emptySpaceIndex = 0
+        var fileIndex = disk.files.endIndex - 1
+
+        var emptySpace: Content? = disk.emptySpaces[emptySpaceIndex]
+        var file: Content? = disk.files[fileIndex]
+        
+        let emptySpaceCount = disk.emptySpaces.count
+        
+        func increaseEmptySpace() {
+            emptySpaceIndex += 1
+            emptySpace = emptySpaceIndex < emptySpaceCount ? disk.emptySpaces[emptySpaceIndex] : nil
+            file = fileIndex >= 0 ? disk.files[fileIndex] : nil
+        }
+        
+        func increaseFile() {
+            fileIndex -= 1
+            file = fileIndex >= 0 ? disk.files[fileIndex] : nil
+            emptySpace = emptySpaceIndex < emptySpaceCount ? disk.emptySpaces[emptySpaceIndex] : nil
+        }
+        
+        while var localEmptySpace = emptySpace, let file = file {
+            if localEmptySpace.position >= file.position {
                 break
             }
             
-            disk[startIndex] = disk[endIndex]
-            disk[endIndex] = .emptySpace
+            if localEmptySpace.size > file.size {
+                files.append(Content(position: localEmptySpace.position, size: file.size, type: file.type))
+                localEmptySpace.size -= file.size
+                localEmptySpace.position += file.size
+                disk.files[fileIndex].size = 0
+
+                emptySpace = localEmptySpace
+                disk.emptySpaces[emptySpaceIndex] = localEmptySpace
+                
+                increaseFile()
+            } else if localEmptySpace.size < file.size {
+                files.append(Content(position: localEmptySpace.position, size: localEmptySpace.size, type: file.type))
+                disk.files[fileIndex].size -= localEmptySpace.size
+                localEmptySpace.size = 0
+                
+                emptySpace = localEmptySpace
+                disk.emptySpaces[emptySpaceIndex] = localEmptySpace
+                
+                increaseEmptySpace()
+            } else {
+                files.append(Content(position: localEmptySpace.position, size: localEmptySpace.size, type: file.type))
+                localEmptySpace.size = 0
+                disk.files[fileIndex].size = 0
+                
+                emptySpace = localEmptySpace
+                disk.emptySpaces[emptySpaceIndex] = localEmptySpace
+                
+                increaseEmptySpace()
+                increaseFile()
+            }
         }
+
+        return files + disk.files.filter({ $0.type != .emptySpace && $0.size > 0 })
     }
     
-    private func task2(_ disk: inout Disk) {
-        var currentContent: Content = .emptySpace
-        var currentLength = 0
+    private func task2(_ disk: inout Disk) -> [Content] {
+        var files: [Content] = []
         
-        let originalDisk = disk
+        var emptySpaceIndex = 0
+        var fileIndex = disk.files.endIndex - 1
         
-        for endIndex in (0..<originalDisk.endIndex).reversed() {
-            let content = originalDisk[endIndex]
-            
-            if currentContent == content {
-                currentLength += 1
-                continue
+        var emptySpace: Content? = disk.emptySpaces[emptySpaceIndex]
+        var file: Content? = disk.files[fileIndex]
+
+        let emptySpaceCount = disk.emptySpaces.count
+        
+        func increaseEmptySpace() {
+            emptySpaceIndex += 1
+            if emptySpaceIndex >= emptySpaceCount {
+                increaseFile()
+            } else {
+                emptySpace = disk.emptySpaces[emptySpaceIndex]
             }
-            
-            if currentContent == .emptySpace {
-                currentContent = content
-                currentLength = 1
-                continue
-            }
-            
-            guard case let .file(id) = currentContent else {
-                fatalError()
-            }
-            
-            var startIndex = 0
-            var emptySpaceLength = 0
-            while startIndex <= endIndex {
-                if case .file(_) = disk[startIndex] {
-                    emptySpaceLength = 0
-                    startIndex += 1
-                    continue
-                }
-                
-                emptySpaceLength += 1
-                if emptySpaceLength == currentLength {
-                    for i in startIndex-currentLength+1...startIndex {
-                        disk[i] = .file(id)
-                    }
-                    for i in endIndex+1...endIndex+currentLength {
-                        disk[i] = .emptySpace
-                    }
-                    break
-                } else {
-                    startIndex += 1
-                }
-            }
-            
-            currentContent = content
-            currentLength = 1
         }
+        
+        func increaseFile() {
+            fileIndex -= 1
+            if fileIndex >= 0 {
+                emptySpaceIndex = 0
+                emptySpace = disk.emptySpaces[emptySpaceIndex]
+                file = disk.files[fileIndex]
+            } else {
+                file = nil
+                emptySpace = nil
+            }
+        }
+        
+        while var localEmptySpace = emptySpace, let file = file {
+            if localEmptySpace.size == 0 {
+                increaseEmptySpace()
+                continue
+            }
+            
+            if localEmptySpace.position >= file.position {
+                increaseFile()
+                continue
+            }
+            
+            if localEmptySpace.size > file.size {
+                files.append(Content(position: localEmptySpace.position, size: file.size, type: file.type))
+                localEmptySpace.size -= file.size
+                localEmptySpace.position += file.size
+                disk.files[fileIndex].size = 0
+                
+                emptySpace = localEmptySpace
+                disk.emptySpaces[emptySpaceIndex] = localEmptySpace
+                
+                increaseFile()
+            } else if localEmptySpace.size < file.size {
+                increaseEmptySpace()
+            } else {
+                files.append(Content(position: localEmptySpace.position, size: file.size, type: file.type))
+                disk.emptySpaces[emptySpaceIndex].size = 0
+                disk.files[fileIndex].size = 0
+                
+                increaseFile()
+            }
+        }
+
+        return files + disk.files.filter({ $0.type != .emptySpace && $0.size > 0 })
     }
 
     private func parse(_ data: String) -> Disk {
-        data.enumerated().flatMap { offset, character in
-            let id = offset/2
-            let size = character.wholeNumberValue!
-            let content: Content = offset%2 == 0 ? .file(id) : .emptySpace
-            return Disk(repeating: content, count: size)
+        var position = 0
+        return data.enumerated().reduce(into: Disk()) { disk, entry in
+            let size = entry.element.wholeNumberValue!
+            let id = entry.offset/2
+            let content = Content(position: position, size: size, type: entry.offset%2 == 0 ? .file(id) : .emptySpace)
+            
+            switch content.type {
+            case .file(_):
+                disk.files.append(content)
+            case .emptySpace:
+                disk.emptySpaces.append(content)
+            }
+            
+            position += size
         }
     }
     
-    typealias Disk = [Content]
-    
-    enum Content: Equatable {
-        case file(Int), emptySpace
-    }
-}
-
-extension Day9.Disk {
-    func checksum() -> Int {
-        self.enumerated().reduce(0) { result, content in
-            if case let .file(id) = content.element {
-                return result + content.offset*id
+    private func checksum(files: [Content]) -> Int {
+        files.reduce(0) { result, content in
+            if case let .file(id) = content.type {
+                return result + (content.position..<content.position+content.size).reduce(0) { $0 + $1*id }
             }
             return result
         }
     }
+    
+    private struct Disk {
+        var files: [Content] = []
+        var emptySpaces: [Content] = []
+    }
+    
+    private struct Content: Equatable {
+        var position: Position
+        var size: Size
+        let type: ContentType
+    }
+    
+    private enum ContentType: Equatable {
+        case file(ID), emptySpace
+    }
+    
+    private typealias ID = Int
+    private typealias Size = Int
+    private typealias Position = Int
 }

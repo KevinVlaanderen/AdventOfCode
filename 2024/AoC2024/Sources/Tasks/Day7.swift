68f19fc3e6d9a98304679c022865559a6c38e04d
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2024-12-12 16:14:56 +0100

    clean up 2024 7 a bit

--- 2024/AoC2024/Sources/Tasks/Day7.swift
+++ 2024/AoC2024/Sources/Tasks/Day7.swift
@@ -1,6 +1,5 @@
 import Foundation
 internal import Algorithms
-internal import SwiftGraph
 import Framework
 
 public struct Day7: Day {
@@ -9,42 +8,17 @@ public struct Day7: Day {
     public init() {}
     
     public func perform(task: Task, data: String, param: P) throws -> Int {
-       return parse(data)
+       parse(data)
             .filter(canSatisfy(operators: param))
             .map { $0.result }
             .reduce(0, +)
-    }
-    
-    func canSatisfy(operators: [Operators]) -> (Equation) -> Bool {
-        return { equation in
-            calculate(total: equation.numbers.first!, target: equation.result, index: 1, numbers: equation.numbers, operators: operators)
-        }
-    }
-    
-    func calculate(total: Int, target: Int, index: Int, numbers: [Int], operators: [Operators]) -> Bool {
-        if index >= numbers.count {
-            return total == target
-        }
-        for op in operators {
-            let newTotal = op.apply(a: total, b: numbers[index])
-            if newTotal == target {
-                return true
-            } else if newTotal > target {
-                continue
-            } else {
-                if calculate(total: newTotal, target: target, index: index+1, numbers: numbers, operators: operators) {
-                    return true
-                }
-            }
-        }
-        return false
     }
     
     nonisolated(unsafe)
     private static let linePattern = /(\d+):((?: \d+)+)/
     
     private func parse(_ data: String) -> [Equation] {
-        return data.matches(of: Day7.linePattern).map { match in
+        data.matches(of: Day7.linePattern).map { match in
             Equation(
                 result: Int(match.output.1)!,
                 numbers: match.output.2.split(whereSeparator: \.isWhitespace).map({
@@ -54,7 +28,26 @@ public struct Day7: Day {
         }
     }
     
-    struct Equation {
+    private func canSatisfy(operators: [Operators]) -> (Equation) -> Bool {
+        { equation in
+            calculate(total: equation.numbers.first!, target: equation.result, index: 1, numbers: equation.numbers, operators: operators)
+        }
+    }
+    
+    private func calculate(total: Int, target: Int, index: Int, numbers: [Int], operators: [Operators]) -> Bool {
+        if total > target {
+            return false
+        } else if index == numbers.endIndex {
+            return total == target
+        }
+        let next = numbers[index]
+        return operators.contains { op in
+            let newTotal = op.apply(a: total, b: next)
+            return calculate(total: newTotal, target: target, index: index+1, numbers: numbers, operators: operators)
+        }
+    }
+    
+    private struct Equation {
         let result: Int
         let numbers: [Int]
     }
@@ -72,8 +65,7 @@ extension Day7.Operators {
         case .multiply:
             return a*b
         case .combine:
-            let total = a*Int(pow(10, Double(b.usefulDigits)))+b
-            return total
+            return a*Int(pow(10, Double(b.usefulDigits)))+b
         }
     }
 }

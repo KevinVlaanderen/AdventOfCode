a6a8542d1410232f975e232897cf10c056f86a14
Kevin Vlaanderen <kevinvlaanderen@gmail.com>
2025-12-14 00:07:25 +0100

    2025 10-1 (collect all options)

--- go/2025/tasks/day10/model.go
+++
@@ -1,7 +0,0 @@
-package day10
-
-type MachineDescription struct {
-	targetLights   int
-	buttons        []int
-	targetJoltages []int
-}
--- go/2025/tasks/day10/tasks.go
+++ go/2025/tasks/day10/tasks.go
@@ -1,59 +1,107 @@
 package day10
 
 import (
+	"aoc/framework/math"
 	"aoc/framework/tasks"
 	"go/types"
+	"math/bits"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/oleiade/lane/v2"
 	"github.com/samber/lo"
 )
 
+type MachineDescription struct {
+	buttons        []uint
+	targetLights   uint
+	targetJoltages []uint
+}
+
 func Task1(data string, _ types.Nil) (result tasks.Result[int]) {
 	machines := Parse(data)
 
 	result.Value = lo.SumBy(machines, func(machine MachineDescription) int {
-		return pressLightButtonsUntilValid(machine.buttons, machine.targetLights)
+		buttonsPressed := pressButtonsUntilValidLights(machine.buttons, machine.targetLights)
+		bestButtonsPressed := lo.MinBy(buttonsPressed, func(a uint, b uint) bool {
+			return bits.OnesCount(a) < bits.OnesCount(b)
+		})
+		return bits.OnesCount(bestButtonsPressed)
 	})
 
 	return
 }
 
-func pressLightButtonsUntilValid(buttons []int, target int) int {
-	best := 0
-	lightsSeen := make(map[int]struct{})
+var machinePattern = regexp.MustCompile(`\[(.+)] (.*) \{(.*)}`)
+var buttonWiringSchematicsPattern = regexp.MustCompile(`\d+`)
+
+func Parse(data string) []MachineDescription {
+	matches := machinePattern.FindAllStringSubmatch(data, -1)
+
+	return lo.Map(matches, func(match []string, index int) MachineDescription {
+		buttonWiringSchematics := lo.Map(strings.Split(match[2], " "), func(schematic string, index int) uint {
+			button := 0
+			for _, wiring := range buttonWiringSchematicsPattern.FindAllString(schematic, -1) {
+				value, _ := strconv.Atoi(wiring)
+				button += math.PowInt(2, value)
+			}
+			return uint(button)
+		})
+
+		indicatorLightDiagram := lo.Reduce([]rune(match[1]), func(agg uint, light rune, index int) uint {
+			if light == '#' {
+				return agg + uint(math.PowInt(2, index))
+			}
+			return agg
+		}, 0)
+
+		joltageRequirements := lo.Map(strings.Split(match[3], ","), func(joltage string, index int) uint {
+			value, _ := strconv.Atoi(joltage)
+			return uint(value)
+		})
+
+		return MachineDescription{
+			buttons:        buttonWiringSchematics,
+			targetLights:   indicatorLightDiagram,
+			targetJoltages: joltageRequirements,
+		}
+	})
+}
+
+func pressButtonsUntilValidLights(buttons []uint, target uint) []uint {
+	answers := make([]uint, 0)
+	buttonPressesSeen := make(map[uint]struct{})
 
 	type State struct {
-		lights  int
-		presses int
+		lights         uint
+		presses        uint
+		buttonsPressed uint
 	}
 
-	queue := lane.NewQueue(State{lights: 0, presses: 0})
+	queue := lane.NewQueue(State{lights: 0, presses: 0, buttonsPressed: 0})
 
 	for queue.Size() > 0 {
 		state, _ := queue.Dequeue()
 
-		if _, ok := lightsSeen[state.lights]; ok {
+		if _, ok := buttonPressesSeen[state.buttonsPressed]; ok {
 			continue
 		}
-
-		lightsSeen[state.lights] = struct{}{}
+		buttonPressesSeen[state.buttonsPressed] = struct{}{}
 
 		if state.lights == target {
-			if best == 0 || state.presses < best {
-				best = state.presses
-			}
-			continue
-		} else if best > 0 && state.presses > best {
+			answers = append(answers, state.buttonsPressed)
 			continue
 		}
 
-		for _, button := range buttons {
+		for index, targetLights := range buttons {
 			newPresses := state.presses + 1
-			newLights := state.lights ^ button
+			newLights := state.lights ^ targetLights
+			newButtonsPressed := state.buttonsPressed | uint(math.PowInt(2, index))
 
-			queue.Enqueue(State{lights: newLights, presses: newPresses})
+			queue.Enqueue(State{lights: newLights, presses: newPresses, buttonsPressed: newButtonsPressed})
 		}
 	}
 
-	return best
+	return answers
 }
